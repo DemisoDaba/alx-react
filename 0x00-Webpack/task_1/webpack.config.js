9e6282160f5728b86fc358c14f165cfd0da4a900
DemisoDaba <demo.nkmt1@gmail.com>
2024-01-29 04:15:26 -0800

    task1

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+};
+
