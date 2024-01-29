9b8ea145ef5adbe13e4353d09f180705cacf5666
DemisoDaba <demo.nkmt1@gmail.com>
2024-01-29 04:37:14 -0800

    try

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,6 +1,8 @@
+
 'use strict';
-import $ from 'jquery';
-import _ from 'lodash';
+import '../css/main.css';
+const $ = require('jquery');
+const _ = require('lodash');
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
@@ -9,10 +11,10 @@ $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 const updateCounter = () => {
-  let times = $('#count').html() || 0;
+  let clicks = $('#count').html() || 0;
   $('button').on('click', () => {
-    times++;
-    $('#count').html(`${times} clicks on the button`);
+    clicks++;
+    $('#count').html(`${clicks} clicks on the button`);
   });
 };
 
